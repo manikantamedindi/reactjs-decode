6e3aca3ce6c50ab929b37dee855d9ef668d3eabc
Manikanta M <manikanta.medindi@non.se.com>
2025-07-17 14:48:43 +0530

    feat: integrate redux-persist for state persistence in the store

--- src/projectFolder/store.js
+++ src/projectFolder/store.js
@@ -1,12 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import counterReducer from "./redux/counterSlice";
 import userReducer from "./redux/userSlice";
 import commentReducer from "./redux/commentSlice";
+import { combineReducers } from "redux";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    newSubscriber: userReducer,
-    newComments: commentReducer,
-  },
+// Combine reducers
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  newSubscriber: userReducer,
+  newComments: commentReducer,
 });
+
+// Persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store with persisted reducer
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// Export persistor
+export const persistor = persistStore(store);
